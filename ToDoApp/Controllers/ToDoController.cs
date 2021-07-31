90bbf830e85e2ada91c088d4a69db59d51b60ad6
Ömer Yıldız <omrfrkyildiz@gmail.com>
2021-07-31 22:05:30 +0300

    ToDoController has been created for the project.

---
+++ ToDoApp/Controllers/ToDoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApp.Models;
+using ToDoApp.Services;
+
+namespace ToDoApp.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private readonly IToDoItemService _toDoItemService;
+
+        public ToDoController(IToDoItemService toDoItemService)
+        {
+            _toDoItemService = toDoItemService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var toDoItems = await _toDoItemService.GetIncompleteItemsAsync();
+            
+            var model = new ToDoViewModel()
+            {
+                ToDoItems = toDoItems
+            };
+
+            return View(model);
+
+        }
+    }
+}
