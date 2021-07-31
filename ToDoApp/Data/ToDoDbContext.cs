ab27be346d1a39a9609fca539ef774cba6f0ccda
Ömer Yıldız <omrfrkyildiz@gmail.com>
2021-07-31 22:06:05 +0300

    DbContext has been created

---
+++ ToDoApp/Data/ToDoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoApp.Models;
+
+namespace ToDoApp.Data
+{
+    public class ToDoDbContext : DbContext
+    {
+
+        public ToDoDbContext(DbContextOptions<ToDoDbContext> options) : base(options) { }
+
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+
+    }
+}
