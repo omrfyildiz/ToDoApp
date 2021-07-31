214e38723411dbc58539c179c94fbffbb20004d5
Ömer Yıldız <omrfrkyildiz@gmail.com>
2021-07-31 22:06:37 +0300

    Model and ViewModel have been created

---
+++ ToDoApp/Models/ToDoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class ToDoItem
+    {
+        public Guid Id { get; set; }
+        public bool IsDone { get; set; }
+        public string Title { get; set; }
+        public DateTimeOffset? StartedAt { get; set; }
+    }
+}
