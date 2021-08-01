60304b80d857ddc0d34756792d85b9364062dd3a
Ömer Yıldız <omrfrkyildiz@gmail.com>
2021-08-02 00:18:13 +0300

    js file has been updated

--- ToDoApp/wwwroot/js/site.js
+++ ToDoApp/wwwroot/js/site.js
@@ -1,4 +1,25 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
 
-// Write your JavaScript code.
+    //addItem function will work when be clicked Add button
+    $('#add-item-button').on('click', addItem);
+});
+
+function addItem() {
+    $('#add-item-error').hide();
+    var newTitle = $('#add-item-title').val();
+
+    $.post('/ToDo/AddItem', { title: newTitle }, function () {
+        window.location = '/ToDo';
+    })
+
+        .fail(function (data) {
+            if (data && data.responseJSON) {
+                var firstError = data.responseJSON[Object.keys(data.responseJSON)[0]];
+                $('#add-item-error').text(firstError);
+                $('#add-item-error').show();
+            }
+        });
+}
+
+
+
