6e29d143e14a42b9b6922dd309094406ef6638d0
Ömer Yıldız <omrfrkyildiz@gmail.com>
2021-08-02 20:00:14 +0300

    New migration has been added and updated again

--- ToDoApp/Migrations/20210802161926_AddItemOwnerId.Designer.cs
+++ ToDoApp/Migrations/20210802161926_AddItemOwnerId.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ToDoApp.Data;
 
 namespace ToDoApp.Migrations
 {
     [DbContext(typeof(ToDoDbContext))]
-    partial class ToDoDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210802161926_AddItemOwnerId")]
+    partial class AddItemOwnerId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
