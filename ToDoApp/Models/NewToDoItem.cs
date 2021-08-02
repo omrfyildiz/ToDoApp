897917bb5a845a94afcef3985ce56a200ec533a8
Ömer Yıldız <omrfrkyildiz@gmail.com>
2021-08-02 20:01:08 +0300

    model has been updated

--- ToDoApp/Models/NewToDoItem.cs
+++ ToDoApp/Models/NewToDoItem.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ToDoApp.Models
 {
-    public record NewToDoItem
+    public class NewToDoItem
     {
         [Required]
         public string Title { get; set; }
